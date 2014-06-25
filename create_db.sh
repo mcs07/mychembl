40190cac87ecbb04c85b1be961b890a83a3ffa85
Matt Swain <m.swain@me.com>
2014-06-25 18:30:53 +0100

    Update script to import ChEBML into PostgreSQL

--- create_db.sh
+++ create_db.sh
@@ -1,40 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-## Download target prediction models
-wget ftp://ftp.ebi.ac.uk/pub/databases/chembl/target_predictions/chembl_18_models.tar.gz
-tar -zxf chembl_18_models.tar.gz
-rm chembl_18_models.tar.gz
+echo "Setting up PostgreSQL database"
+initdb /usr/local/var/postgres -E utf8
+# Backup postgresql.conf
+NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+cp "/usr/local/var/postgres/postgresql.conf" "/usr/local/var/postgres/postgresql.conf-$NOW"
+# Update shared_buffers and work_mem in postgresql.conf
+sed -i '' "s/^#*shared_buffers \=.*/shared_buffers = 1024MB/" "/usr/local/var/postgres/postgresql.conf"
+sed -i '' "s/^#*work_mem \=.*/work_mem = 128MB/" "/usr/local/var/postgres/postgresql.conf"
 
-## Install ChEMBLdb
-wget ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/releases/chembl_18/chembl_18_postgresql.tar.gz
-tar zxf chembl_18_postgresql.tar.gz
+echo "Dowloading ChEMBL database"
+cd ~/mychembl
+wget -N ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/releases/chembl_18/chembl_18_postgresql.tar.gz
+tar -zxf chembl_18_postgresql.tar.gz
 
+echo "Importing ChEMBL into PostgreSQL"
+pg_ctl -w -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
 createdb chembl_18
-
-cd chembl_18_postgresql/
-psql chembl_18 < chembl_18.pgdump.sql
-
-cd ..
-
-rm chembl_18_postgresql.tar.gz
-
-echo "chemblvm" | sudo -S curl -o /etc/postgresql/9.1/main/postgresql.conf https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_postgresql.conf
-echo "chemblvm" | sudo -S curl -o /etc/postgresql/9.1/main/pg_hba.conf https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_pg_hba.conf
-echo "chemblvm" | sudo -S curl -o /etc/sysctl.conf https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_sysctl.conf
-
-echo "chemblvm" | sudo -S service postgresql restart
-
-psql --username=chembl -d chembl_18 -c "create extension rdkit;"
+psql chembl_18 < chembl_18_postgresql/chembl_18.pgdump.sql
+rm -r chembl_18_postgresql
+psql -d chembl_18 -c "create extension rdkit;"
 wget https://raw.githubusercontent.com/chembl/mychembl/master/indexes.sql
-psql --username=chembl -d chembl_18 -a -f indexes.sql
+psql -d chembl_18 -a -f indexes.sql
 rm indexes.sql
-
 wget https://raw.githubusercontent.com/chembl/mychembl_webapp/master/sql/webapp.sql
-psql --username=chembl -d chembl_18 -a -f webapp.sql
+psql -d chembl_18 -a -f webapp.sql
 rm webapp.sql
-
-psql --username=chembl -d chembl_18 -c "create user mychembl password 'read';"
-psql --username=chembl -d chembl_18 -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO mychembl;"
-
-cd /home/chembl/
-rm chembl_18_postgresql/ -rf
+psql -d chembl_18 -c "create user mychembl password 'read';"
+psql -d chembl_18 -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO mychembl;"

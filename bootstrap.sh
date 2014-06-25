78ad52c311588570112b8ce30618fda6ad635357
Matt Swain <m.swain@me.com>
2014-06-25 18:24:14 +0100

    Modify existing PostgreSQL conf instead of overwriting

--- bootstrap.sh
+++ bootstrap.sh
@@ -96,6 +96,15 @@ fi
 # Start Apache
 sudo apachectl restart
 
+echo "Setting up PostgreSQL database"
+initdb /usr/local/var/postgres -E utf8
+# Backup postgresql.conf
+NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+cp "/usr/local/var/postgres/postgresql.conf" "/usr/local/var/postgres/postgresql.conf-$NOW"
+# Update shared_buffers and work_mem in postgresql.conf
+sed -i '' "s/^#*shared_buffers \=.*/shared_buffers = 1024MB/" "/usr/local/var/postgres/postgresql.conf"
+sed -i '' "s/^#*work_mem \=.*/work_mem = 128MB/" "/usr/local/var/postgres/postgresql.conf"
+
 sudo curl -o /etc/phppgadmin/apache.conf https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_phppgadmin_httpd.conf
 sudo curl -o /etc/apache2/conf.d/phppgadmin https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_phppgadmin_httpd.conf
 

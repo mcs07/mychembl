90ac24f23709289252f022691f39b9a6d59e5395
Matt Swain <m.swain@me.com>
2014-06-26 12:12:25 +0100

    Consolidate python installs

--- setup_python.sh
+++ setup_python.sh
@@ -20,7 +20,13 @@ pip install -U mpld3
 pip install -U service_identity
 pip install -U paste
 pip install -U psycopg2
+pip install -U django
+pip install -U django-cors-headers
 pip install -U pillow
 pip install -U networkx
 pip install -U lxml
+
+# Install ChEMBL packages
 pip install -U chembl_webresource_client
+pip install -U chembl_webservices
+pip install -U chembl_compatibility
